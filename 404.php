4d98a5997d46427a11a02832c2ed9d8365afceb0
dolgiyseryoga@gmail.com <95541347+dolgiyseryoga@users.noreply.github.com>
2022-05-12 23:36:05 +0300

    404

---
+++ 404.php
@@ -0,0 +1,42 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> 404 Такой страницы нет! </title>
+    <!-- animation -->
+    <link rel="stylesheet" type="text/css" href="css/animations.css">
+    <link rel="stylesheet" type="text/css" href="css/ihover.css">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="responsive.css">
+</head>
+
+<body>
+
+<section class="shr-hero-area" id="hero">
+        <div class="container">
+            <div class="row animatedParent animateOnce">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                <h1> Такой страницы не существует!<h1>               
+                </span class="shr-color" >Будь внимательнее, дружище!</span>
+                    <br></br>
+                    <h2></span class="shr-color"><a href="index.php">Вернёмся на главную?</a></span></h2>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 shr-dis-none">
+                    <div class="shr-hero-content-img animated fadeInUpShort slow delay-250">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
